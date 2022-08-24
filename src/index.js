48995674a898bdaee35e420e95abd3618ae4400e
juan lentino <nuncaparesdecrear@gmail.com>
2022-08-23 23:24:08 -0500

    Integrando Redux DevTools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const store = createStore(pokemonsReducer)
+const store = createStore(
+    pokemonsReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
