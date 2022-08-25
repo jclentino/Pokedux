24bb5c7481c457ec4b576095b31284e0e2e0739e
juan lentino <nuncaparesdecrear@gmail.com>
2022-08-24 20:11:20 -0500

    Añadiendo un Loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getPokemonsWithDetails } from './actions';
+import { getPokemonsWithDetails, setLoading } from './actions';
 import Searcher from './components/Searcher'
 import PokemonList from './components/PokemonList';
-import { Col } from 'antd'
+import { Col, Spin } from 'antd'
 import { getPokemon } from './api';
 import pokedux from './statics/pokedux.svg'
 import './App.css';
@@ -12,13 +12,16 @@ import './components/PokemonList.css'
 
 const App = () => {
   const pokemons = useSelector(state => state.pokemons)
+  const loading = useSelector(state => state.loading)
   const dispatch = useDispatch()
 
 
   useEffect(()=> {
+    dispatch(setLoading(true))
     const fetchPokemons = async ()=> {
       const pokemonsRes = await getPokemon()
       dispatch(getPokemonsWithDetails(pokemonsRes))
+      dispatch(setLoading(false))
     }
 
     fetchPokemons()
@@ -32,7 +35,14 @@ const App = () => {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons} />
+      {
+        loading ? 
+        <Col offset={12}>
+          <Spin spinning size='large'  />
+        </Col> :
+        <PokemonList pokemons={pokemons} />
+      }
+      
     </div>
   );
 }
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import { getPokemonDetail } from "../api";
-import { SET_POKEMONS } from "./types";
+import { SET_POKEMONS, SET_LOADING } from "./types";
 
 export const setPokemons = (payload)=> ({
     type: SET_POKEMONS,
@@ -10,3 +10,8 @@ export const getPokemonsWithDetails = (pokemons = [])=> async (dispatch)=> {
     const pokemonsDetailed = await Promise.all(pokemons.map(pokemon => getPokemonDetail(pokemon)))
     dispatch(setPokemons(pokemonsDetailed))
 }
+
+export const setLoading = (payload) => ({
+    type: SET_LOADING,
+    payload
+})
