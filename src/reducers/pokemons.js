bba8cd0b335578138416874146792f3d1905834e
juan lentino <nuncaparesdecrear@gmail.com>
2022-08-25 20:44:22 -0500

    Agregando Inmutabilidad a nuestra Pokedux

--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,26 +1,27 @@
+import { fromJS } from "immutable";
 import { SET_FAVORITE, SET_LOADING, SET_POKEMONS } from "../actions/types";
 
-const initialState = {
+const initialState = fromJS({
     pokemons: [],
     loading: false,
-}
+})
 
 export const pokemonsReducer = (state = initialState, action)=> {
     switch(action.type){
         case SET_POKEMONS:
-            return { ...state, pokemons: action.payload } 
+            return state.setIn(['pokemons'], fromJS(action.payload)) 
         case SET_FAVORITE:
-            const newPokemonList = [ ...state.pokemons ]
-            const pokemonIndex = newPokemonList.findIndex(pokemon => pokemon.id === action.payload.pokemonId)
+            const pokemonIndex = state.get('pokemons')
+                .findIndex(pokemon => pokemon.get('id') === action.payload.pokemonId)
 
             if (pokemonIndex < 0){
                 return state
             }
 
-            newPokemonList[pokemonIndex].favorite = !newPokemonList[pokemonIndex].favorite 
-            return { ...state, pokemons: newPokemonList }
+            const isFavorite = state.getIn(['pokemons', pokemonIndex, 'favorite'])
+            return state.setIn(['pokemons', pokemonIndex, 'favorite'], !isFavorite)
         case SET_LOADING:
-                return { ...state, loading: action.payload}
+                return state.setIn(['loading'], action.payload)
         default: 
             return state 
     }
